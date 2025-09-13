e399c62d6da9917f550b9ac8abb548efa9499942
Marz270 <nicomars270@gmail.com>
2025-09-12 22:26:33 -0300

    first commit

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+require("dotenv").config();
+const indexRoutes = require("./routes/index.routes");
+const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    message: "Something went wrong!",
+    error: err.message,
+  });
+});
+
+// Routes
+app.use("/", indexRoutes);
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
